13bd7dac798bf777a90dda175d5c0ce367779434
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2018-09-25 08:51:48 +0700

    Tambahan animasi dan jquery filter

--- Tugas4.js
+++ Tugas4.js
@@ -1,14 +1,30 @@
 $(document).ready(() => {
+    
 
     $('.data').on('mouseenter', event => {
         $('.data').css('color', 'black');
-    }
-    );
+
+        $('.data').animate({
+            fontSize: '100px'
+        }, 200)
+    });
 
     $('.data').on('mouseleave', event => {
         $('.data').css('color', 'white');
+        $('.data').animate({
+            fontSize: '50px'
+        }, 200)
+
     }
+    
+
 );
+    $("#caridata").on("keyup", function () {
+        $("#iniTable tr").filter(function () {
+            $(this).toggle($(this).text().indexOf($('#caridata').val()) >= 0)
+        });
+    });
+
     $('.image').on('click', event=>
     {$(event.currentTarget).hide();
     }).on('mouseenter', event => {
